e5beff1011bbe6f38f069edc649ac8b753c5d205
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-04-28 00:18:33 +0300

    - handel UI regesiter screen.

---
+++ lib/features/sign_up/presentation/widgets/create_account_page.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  @override
+  _CreateAccountPageState createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  bool agree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: ListView(
+          children: [
+            SizedBox(height: 60),
+            Text(
+              "Create Account",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Fill your information below or register\nwith your social account.",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Name",
+                hintText: "John Doe",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "example@gmail.com",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: "***********",
+                suffixIcon: Icon(Icons.visibility_off),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Checkbox(
+                  value: agree,
+                  activeColor: Color(0xFF936e5f),
+                  onChanged: (value) {
+                    setState(() {
+                      agree = value!;
+                    });
+                  },
+                ),
+                Text("Agree with "),
+                GestureDetector(
+                  onTap: () {
+                   
+                  },
+                  child: Text(
+                    "Terms & Condition",
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.brown),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF936e5f),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 15),
+              ),
+              child: Text("Sign Up", style: TextStyle(fontSize: 18)),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Expanded(child: Divider(thickness: 1)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text("Or sign up with"),
+                ),
+                Expanded(child: Divider(thickness: 1)),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // children: [
+              //   _buildSocialIcon('assets/apple.png'),
+              //   _buildSocialIcon('assets/google.png'),
+              //   _buildSocialIcon('assets/facebook.png'),
+              // ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account? "),
+                GestureDetector(
+                  onTap: () {
+                  
+                  },
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.brown),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialIcon(String imagePath) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Image.asset(
+        imagePath,
+        height: 30,
+        width: 30,
+      ),
+    );
+  }
+}
---
+++ lib/features/sign_up/presentation/widgets/sign_in_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: ListView(
+          children: [
+            SizedBox(height: 60),
+            Text(
+              "Sign In",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Hi! Welcome back, you’ve been missed",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "example@gmail.com",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: "***********",
+                suffixIcon: Icon(Icons.visibility_off),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                onTap: () {
+        
+                },
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.brown),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF936e5f),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 15),
+              ),
+              child: Text("Sign In", style: TextStyle(fontSize: 18)),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Expanded(child: Divider(thickness: 1)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text("Or sign in with"),
+                ),
+                Expanded(child: Divider(thickness: 1)),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildSocialIcon('assets/apple.png'),
+                _buildSocialIcon('assets/google.png'),
+                _buildSocialIcon('assets/facebook.png'),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don’t have an account? "),
+                GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.brown),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialIcon(String imagePath) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Image.asset(
+        imagePath,
+        height: 30,
+        width: 30,
+      ),
+    );
+  }
+}
